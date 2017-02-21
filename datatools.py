4f89cc53a5cf1a9cf680f4c7001e2630dba35c8f
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-02-21 13:48:35 -0800

    Cleaning up code + fixing some issues
    Former-commit-id: ed8068b0e2788549779da6e71f1804c04b1e8a38

--- datatools.py
+++ datatools.py
@@ -149,21 +149,14 @@ def equalize_distribution(xs, ys, coords, sizes):
                 for idx in range(needed):
                     i = idx % len(images)
                     image = images[i]
-                    # Can look at: https://github.com/Hvass-Labs/TensorFlow-Tutorials/blob/master/06_CIFAR-10.ipynb
-                    #   for ideas for image transformations
-                    #image = crop(image, coords[i], sizes[i])
-                    #image = addnoise(image, 5)
-                    #image = rotate(image)
-                    #image = sharpen_blur(image)
-                    #image = push_away(image, (img_size, img_size, num_channels))
-                    #image = change_perspective(image)
-                    image = transform_image(image, 20, 10, 5)
-                    image = resize(image, img_size) # Get image back to required size, in case that changed
+                    
+                    # TODO: Move transformation code over to another location in the source
                     
                     more_images.append(image)
                     more_labels.append(cls)
         
     return more_images, more_labels
+
 
 def print_distribution(xs, ys):
     num_images = len(xs)
