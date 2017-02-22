ebae84f90ce8314aad173db8c42bf1099589c7b3
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-02-22 01:16:09 -0800

    Numerous documentation changes, and supporting code changes for better illustrations, and some file system reordering
    Former-commit-id: a39bbbf18f77ad993ba00358003e15d6bd70f864

--- plottools.py
+++ plottools.py
@@ -1,30 +1,45 @@
-import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Function used to plot 9 input_images in a 3x3 grid, and writing the true and predicted classes below each image.
-def plot_images(images, actual_labels, predictions=None):
-    assert len(images) <= len(actual_labels) <= 9
+def plot_images(images, actual_labels, predictions=None, titles=None):
     img_shape = images[0].shape
-    fig, axes = plt.subplots(3, 3)
-    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+    h=int(round(np.sqrt(len(images))))
+    v=int(np.ceil(len(images)/h))
+#     diff = (h*v) - len(images)
+#     sample = np.zeros((20, 20, 3), dtype=np.uint8)
+#     for _ in range(diff):
+#         sections[0].append(Image("--Blank--", sample, None))
+#     sections = np.reshape(np.array(sections), (v,h))
+
+    fig, axes = plt.subplots(v, h,figsize=(11,11))
+    fig.subplots_adjust(hspace=0.5, wspace=0.5)
     for i, ax in enumerate(axes.flat):
-        ax.imshow(images[i].reshape(img_shape), cmap='binary')
-        if predictions is None:
-            xlabel = "True: {0}".format(actual_labels[i])
+        if i < len(images):
+            ax.imshow(images[i].reshape(img_shape), cmap='binary')
+            if predictions is None:
+                xlabel = "True: {0}".format(actual_labels[i])
+            else:
+                xlabel = "True: {0}, Predicted: {1}".format(actual_labels[i], predictions[i])
+            if titles is not None:
+                ax.title(titles[i])
+            ax.set_xlabel(xlabel)
+            ax.set_xticks([])
+            ax.set_yticks([])
         else:
-            xlabel = "True: {0}, Pred: {1}".format(actual_labels[i], predictions[i])
-        ax.set_xlabel(xlabel)
-        ax.set_xticks([])
-        ax.set_yticks([])
+            ax.imshow(np.zeros(img_shape), cmap='binary')
+            ax.set_xticks([])
+            ax.set_yticks([])
+            
     plt.show()
 
 # Function for plotting examples of input_images from the test-set that have been mis-classified.
 def plot_example_errors(data, predictions, gradings):
-    incorrect = (gradings == False)
+    incorrect = [i for (i,grade) in enumerate(gradings) if grade == False]
     images = data.images[incorrect] #images = [ data.images[i] for i in range(data.count) where incorrect[i]]
     predictions = predictions[incorrect]
     actuals = data.labels[incorrect]
-    plot_images(images=images[0:9], actual_labels=actuals[0:9], predictions=predictions[0:9])
+    plot_images(images=images, actual_labels=actuals, predictions=predictions)
 
 # Helper-function for plotting an image.
 def plot_image(image):
